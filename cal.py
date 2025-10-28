87f63af3292df476e057ba8ff8ee1363256016a0
vaishnavisuresh588 <vaishnavisuresh588@gami.com>
2025-10-28 11:53:24 +0530

    calculator

---
+++ cal.py
@@ -0,0 +1,24 @@
+# Simple arithmetic operations
+
+a = 10
+b = 5
+
+# Addition
+add = a + b
+print("Addition:", add)
+
+# Subtraction
+sub = a - b
+print("Subtraction:", sub)
+
+# Multiplication
+mul = a * b
+print("Multiplication:", mul)
+
+# Division
+div = a / b
+print("Division:", div)
+
+# Modulus
+mod = a % b
+print("Modulus:", mod)
